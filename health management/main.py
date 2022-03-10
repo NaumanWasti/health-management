dc4cdabbe7bc1c7e2c43c800a707f4f98ed65bb9
Syed Muhammad Nauman <87071367+NaumanWasti@users.noreply.github.com>
2022-03-10 23:01:25 +0500

    Add files via upload

---
+++ health management/main.py
@@ -0,0 +1,73 @@
+
+
+# def getdata():
+#     import datetime
+#     return datetime.datetime.now()
+# t=str(getdata())
+# L=1
+# def details():
+#     A=input("whose details u want?")
+#     B=input("ex or food?")
+#     f=open(A+"_"+B+".txt")
+#     print(f.read())
+
+        
+
+# D=input("do you want details or log the value?")
+# if D=="log":
+#     pass
+# else:
+#     while L!=0:
+#         details()
+#         L=int(input("type 0 for exit and 1 for details"))
+
+
+# Quest=input("enter food or exercise \n")
+# k=1
+# count=1
+
+
+# while k!=0:
+#     if Quest=="exercise":
+        
+#         Name=input("who has done exercise?")
+#         f=open(Name+"_ex.txt","a")
+#         if count==1:
+#             f.write("Time \t \t \t \t \t \texercise \n")
+#             count+=1
+            
+#         ex=input("what exercise he did?")
+#         f.write(t)
+#         f.write("\t"+ex+"\n")
+#         f.close()
+#         k=int(input("type 0 for exit and 10 for details"))
+#         if k==0:
+#             print("breaking")
+#             break
+#         if k==10:
+#             details()
+#             break
+#     if Quest=="food":
+#         Name=input("who has taken food?")
+#         f=open(Name+"_food.txt","a")
+#         if count==1:
+#             f.write("Time \t \t \t \t \t \tfood \n")
+#             count+=1
+#         fd=input("what food he has eaten?")
+#         f.write(t)
+#         f.write("\t"+fd+"\n")
+#         f.close()
+#         k=int(input("type 0 for exit and 10 for details"))
+#         if k==0:
+#             print("breaking")
+#             break
+#         if k==10:
+#             details()
+#             break
+#     else:
+#         print("enter correct")
+#         break
+# Health Management System
+# 3 clients - Harry, Rohan and Hammad
+
+
